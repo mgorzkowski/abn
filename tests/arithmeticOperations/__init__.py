c3cadbdce5f9e9d76324cc65c4ba02d3ee9fcc02
Maciej Gorzkowski <dhmaciek@gmail.com>
2019-02-03 15:42:54 +0100

    changes in testing part

--- tests/arithmeticOperations/__init__.py
+++ tests/arithmeticOperations/__init__.py
@@ -1,2 +1,1 @@
 from arithmeticOperations import *
-from bitOperations import *
--- tests/run.py
+++ tests/run.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python
 import unittest
+import traceback
 import os
+from termcolor import colored
 
-from testCases import *
+import arithmeticOperations
+import bitwiseOperations
+
+def getSuitesDictionary():
+    testModules = [arithmeticOperations, bitwiseOperations]
+    suites = {}
+    for testModule in testModules:
+        suites[testModule] = unittest.TestSuite()
+        suites[testModule].addTests(unittest.TestLoader().loadTestsFromModule(testModule))
+    return suites
+
+def runSuites(suiteDictionary, verbosity = 1):
+    runner = unittest.TextTestRunner(verbosity=verbosity)
+    results = []
+    for module, suite in suiteDictionary.iteritems():
+        print '\nRunnig tests from ' + module.__name__ + ':'
+        result = runner.run(suite)
+        results.append(result)
+    return results
+
+def printResults(results):
+    print '\n'
+    succesful = True
+    for result in results:
+        result = unittest.TestResult()
+        print result
+        if not result.wasSuccessful():
+            succesful = False
+    print '\n'
+    if succesful:
+        print colored('[Success]', 'green')
+    else:
+        print '[Errors or fails occured]'
 
 if __name__ == '__main__':
     if os.path.exists('../bin/shared/libabn.so'):
-        unittest.main()
+        try:
+            suiteDictionary = getSuitesDictionary()
+            results = runSuites(suiteDictionary)
+            printResults(results)
+        except:
+            traceback.print_exc()
     else:
         print 'There is no shared object found'
