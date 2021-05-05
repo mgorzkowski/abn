ec212044377f49030b6bd90991a13d39dfe311ce
Maciej Gorzkowski <dhmaciek@gmail.com>
2021-05-05 13:36:42 +0200

    Update example/sample/setupt.sh

--- examples/sample/setup.sh
+++ examples/sample/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+script_name="setup.sh"
+
 die() {
-    echo $1
-    exit -1
+    echo >&2 "$script_name: $@"
+	exit 1
 }
+
 
 [[ ! -z $ABN_ROOT ]] || die "There is no defined ABN_ROOT env variable. Run 'source ./env.sh' in the root abn directory."
 ln -s $ABN_ROOT 3rd_party/abn
